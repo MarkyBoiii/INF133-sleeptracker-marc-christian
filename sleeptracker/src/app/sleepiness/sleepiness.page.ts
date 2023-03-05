0422b4966796c4b360cb9d8c93748499bb7fcf92
Christian <chrguarin20@gmail.com>
2023-03-05 05:41:10 -0800

    FIXED SLEEPINESS RATING BUTTONS, ADDED DONE BUTTON
    sleepiness will now only be logged once done button is hit — then sends back to home

--- sleeptracker/src/app/sleepiness/sleepiness.page.ts
+++ sleeptracker/src/app/sleepiness/sleepiness.page.ts
@@ -8,39 +8,37 @@ import { SleepService } from '../services/sleep.service';
   styleUrls: ['./sleepiness.page.scss'],
 })
 export class SleepinessPage implements OnInit {
+  private static sleepiness:StanfordSleepinessData;
 
   constructor(public sleepService:SleepService) { }
 
   ngOnInit() {
   }
 
-  // s1-s7 are functions that log the sleepiness from a scale of 1-7 in the sleepService
+  // s1-s7 are functions that set the sleepiness variable to a StanfordSleepinessData object on a scale from 1-7
   s1() {
-    let sleepiness = new StanfordSleepinessData(1);
-    this.sleepService.logSleepinessData(sleepiness);
+    SleepinessPage.sleepiness = new StanfordSleepinessData(1);
   }
   s2() {
-    let sleepiness = new StanfordSleepinessData(2);
-    this.sleepService.logSleepinessData(sleepiness);
+    SleepinessPage.sleepiness = new StanfordSleepinessData(2);
   }
   s3() {
-    let sleepiness = new StanfordSleepinessData(3);
-    this.sleepService.logSleepinessData(sleepiness);
+    SleepinessPage.sleepiness = new StanfordSleepinessData(3);
   }
   s4() {
-    let sleepiness = new StanfordSleepinessData(4);
-    this.sleepService.logSleepinessData(sleepiness);
+    SleepinessPage.sleepiness = new StanfordSleepinessData(4);
   }
   s5() {
-    let sleepiness = new StanfordSleepinessData(5);
-    this.sleepService.logSleepinessData(sleepiness);
+    SleepinessPage.sleepiness = new StanfordSleepinessData(5);
   }
   s6() {
-    let sleepiness = new StanfordSleepinessData(6);
-    this.sleepService.logSleepinessData(sleepiness);
+    SleepinessPage.sleepiness = new StanfordSleepinessData(6);
   }
   s7() {
-    let sleepiness = new StanfordSleepinessData(7);
-    this.sleepService.logSleepinessData(sleepiness);
+    SleepinessPage.sleepiness = new StanfordSleepinessData(7);
+  }
+
+  done() {
+    this.sleepService.logSleepinessData(SleepinessPage.sleepiness);
   }
 }
